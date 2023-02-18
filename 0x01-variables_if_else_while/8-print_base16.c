3eafd73f21b61e08145430c3cfa336294928123b
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-02-17 16:54:36 -0800

    7-print_tebahpla

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,6 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
  */
 
 int main(void)
@@ -12,9 +11,9 @@ int main(void)
 	int d;
 
 	c = 'a';
-	d - 0;
+	d = 0;
 	while
-		d < 10 {
+		(d < 10) {
 			putchar(d + '0');
 			d++;
 		}
@@ -23,5 +22,6 @@ int main(void)
 			putchar(c);
 			c++;
 		}
-	putchar('\n'):
-		return (0);
+	putchar('\n');
+	return (0);
+}
