1104f23301e07fe809713b4de2140df113da4452
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-02-18 16:48:59 -0800

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -9,10 +11,11 @@
 int main(void)
 {
 	int p, q;
+
 	for (p = 0; p <= 98; p++)
 	{
 
-	ifor (q = p + 1; q<= 99; q++)
+	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
