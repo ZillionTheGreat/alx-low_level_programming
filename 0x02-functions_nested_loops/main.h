066d8183c2d80aca47559c4c867ab63c62befc69
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-02-21 14:32:35 -0800

    3-islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifindef MAIN_H
-#define MAIN_H
-
 int _putchar(char c)
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,6 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-
-#endif
+void print_to_98(int n)
