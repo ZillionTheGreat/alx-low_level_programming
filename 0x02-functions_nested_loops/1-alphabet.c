b64fe28e3a78f72d16b40682421c7b4b0e7f5335
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-02-22 02:59:59 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - Print the alphabet
- * Returt: void
+ * Return: void
  */
 
 void print_alphabet(void)
@@ -10,6 +10,7 @@ void print_alphabet(void)
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
 	{
+
 	_putchar(c);
 	}
 	_putchar('\n');
