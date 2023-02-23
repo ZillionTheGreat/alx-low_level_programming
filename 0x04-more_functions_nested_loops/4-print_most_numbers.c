c5ba1fcdec4bb1c67481a0bce90bb16f5c0d35c6
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-02-23 12:51:16 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from zero to nine excluding 2 and 4
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+	for (c = 9; c <= 9; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
+}
