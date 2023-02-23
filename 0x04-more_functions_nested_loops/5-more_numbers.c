72954ce9d0d587511342183e81820ec90dd2ac87
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-02-23 13:12:54 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers 10 times from 0 to 14
+ * Return: 0 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for ( x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y >9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
