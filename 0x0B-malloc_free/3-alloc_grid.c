b31fd79737251b9a97b6784cf11a395f362bf20e
ZillionTheGreat <lebogang.sehlapelo1@gmail.com>
2023-03-14 11:08:58 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,14 +17,14 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	mee = malloc(sizeof(int *) * height);
-	
+
 	if (mee == NULL)
 		return (NULL);
 
 	for (x = 0; x < height; x++)
 	{
 		mee[x] = malloc(sizeof(int) * width);
-		
+
 		if (mee[x] == NULL)
 		{
 			for (; x >= 0; x--)
